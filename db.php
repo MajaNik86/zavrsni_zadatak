f14ec080e44846e9596325d0403661999707c3ed
MajaNik86 <majanik86@gmail.com>
2022-06-07 05:16:40 +0200

    Zadatak 4 i zadatak 3 dodata ista boja na hover

---
+++ db.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "Mateja2019";
+$dbname = "blog";
+
+try {
+    $connection = new PDO(
+        "mysql:host=$servername;dbname=$dbname",
+        $username,
+        $password
+    );
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+function fetch($sql, $connection, $isFetchAll = false)
+{
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+    if ($isFetchAll) {
+        return $statement->fetchAll();
+    }
+
+    return $statement->fetch();
+}
